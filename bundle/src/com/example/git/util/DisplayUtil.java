586296e0316036059a0afcc1f1508070fba88b33
Techaniti Chantakien <techaniti.chantakien@rail.bombardier.com>
2017-03-21 14:07:18 +0700

    Add utility function
    Change-Id: I11da5f83ef10b808adfb169bfa907ecec6a0ddea

---
+++ bundle/src/com/example/git/util/DisplayUtil.java
@@ -0,0 +1,16 @@
+package com.example.git.util;
+
+public class DisplayUtil {
+	
+	/*
+	 * Default constructor
+	 */
+	public DisplayUtil(){
+		
+	}
+	
+	public static String getHelloRepository(){
+		return "Hello Repository";
+	}
+
+}
