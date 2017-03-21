8d1b4c54fa5c7d01f56cc9d9db1b72043a0fc645
Techaniti Chantakien <techaniti.chantakien@rail.bombardier.com>
2017-03-21 14:12:22 +0700

    Update Display.java to use function of DisplayUtil.java
    Change-Id: Iccc1af121a29ced39933ef9ed631c963827efcab

--- bundle/src/com/example/git/display/Display.java
+++ bundle/src/com/example/git/display/Display.java
@@ -1,10 +1,13 @@
 package com.example.git.display;
 
+import com.example.git.util.DisplayUtil;
+
 public class Display {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Thailand");
+		System.out.println(DisplayUtil.getHelloRepository());
 
 	}
 
